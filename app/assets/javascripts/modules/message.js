c0a122941bdf389bf652dd498843ce3c37bff0bf
nononobiii <tefu.0353@gmail.com>
2020-08-23 16:20:22 +0900

    自動更新を実装しました。

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="message-list">
+        `<div class="message-list" data-message-id=${message.id}>
           <div class="message-list__info">
             <div class="message-list__info__name">
               ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
       return html;
     } else {
       let html = 
-      `<div class="message-list">
+      `<div class="message-list" data-message-id=${message.id}>
         <div class="message-list__info">
           <div class="message-list__info__name">
             ${message.user_name}
@@ -40,7 +41,6 @@ $(function(){
     };
   }
 
-
   $('.form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -56,12 +56,37 @@ $(function(){
     .done(function(data){
       let html = buildHTML(data);
       $('.main-chat__message-list').append(html);
-      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
       $('form')[0].reset();
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
       $('.form__submit-btn').prop('disabled', false);
     })
     .fail(function(){
       alert("メッセージ送信に失敗しました");
+      $('.form__submit-btn').prop('disabled', false);
     });
   });
+
+  // let reloadMessages = function() {
+  //   let last_message_id = $('.message-list:last').data("message-id") || 0;
+  //   $.ajax({
+  //     url: "api/messages",
+  //     type: 'get',
+  //     dataType: 'json',
+  //     data: {id: last_message_id}
+  //   })
+  //   .done(function(message) {
+  //     if (message.length !== 0) {
+  //       let insertHTML = '';
+  //       $.each(messages, function(i ,message) {
+  //         insertHTML += buildHTML(message)
+  //       });
+  //       $('.main-chat__message-list').append(insertHTML);
+  //       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+  //     }
+  //   })
+  //   .fail(function() {
+  //     alert('error');
+  //   });
+  // };
+  // setInterval(reloadMessages, 7000);
 });
